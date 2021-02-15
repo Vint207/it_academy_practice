6fb17e540b853b5c4ac0eca0656c140404dc7afd
Admin <Admin@BEST-KOMP>
2021-02-15 22:01:58 +0300

    Added GoodDay

---
+++ GoodDay/GoodDay/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace GoodDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (DateTime.Now.Hour > 8 & DateTime.Now.Hour < 12)
+            {
+                WriteLine("Good morning, guys");
+            }
+            else if (DateTime.Now.Hour > 11 & DateTime.Now.Hour < 15)
+            {
+                WriteLine("Good day, guys");
+            }
+            else if (DateTime.Now.Hour > 14 & DateTime.Now.Hour < 23)
+            {
+                WriteLine("Good evening, guys");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
