19f8a05990343b9f1155e6892317fd892684e078
Admin <Admin@BEST-KOMP>
2021-02-15 22:04:14 +0300

    Added LettersCount

---
+++ GoodDay/LettersCount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace LettersCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            int count;
+
+            word = ReadLine();
+            count = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a')
+                {
+                    count++;
+                }
+            }
+            WriteLine(count);
+        }
+    }
+}
