c810ba2aec898e857e9f2fb99f36b779632ebba5
Admin <Admin@BEST-KOMP>
2021-02-22 20:42:00 +0300

    Added Task2

---
+++ GoodDay/Practice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateCollection();
+        }
+
+        static void CreateEmptyArray()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void CreateCollection()
+        {
+
+
+            ArrayList list = new ArrayList() { 32, 'A', "Hello" };
+           
+            foreach (var item in list)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine();
+
+            list[0] = (int)list[0] + 10;
+            list[2] = $"{list[2]}, Guys!";
+
+            foreach (var item in list)
+            {
+                Console.Write($" {item}");
+            }
+        }
+
+        
+    }
+}
