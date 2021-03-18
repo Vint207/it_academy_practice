a90818b2e450054a1c239f54c8e4ea935c53b464
Vint207 <zhlsgt@gmail.com>
2021-03-18 17:00:00 +0300

    3

---
+++ GoodDay/lecture_11/Figures.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lecture_11
+{
+    class Figures<T> where T : Figure
+    {
+        public List<T> figures;
+
+        public Figures() { figures = new List<T>(); }
+    }
+}
--- GoodDay/lecture_11/Program.cs
+++ GoodDay/lecture_11/Program.cs
@@ -6,39 +6,17 @@ namespace lecture_11
     {
         static void Main()
         {
+            Figures<Figure> figures = new Figures<Figure>();
 
-            object[] figures = new object[5];
+            figures.figures.Add(new Rectangle(10, 10));
+            figures.figures.Add(new Rectangle(20, 20));
+            figures.figures.Add(new Triangle(10, 10));
+            figures.figures.Add(new Triangle(20, 20));
+            figures.figures.Add(new Circle(10));
 
-            figures[0] = new Rectangle(10, 10);
-            figures[1] = new Rectangle(20, 20);
-            figures[2] = new Triangle(10, 10);
-            figures[3] = new Triangle(20, 20);
-            figures[4] = new Circle(10);
-
-            foreach (var item in figures)
-            {
-                System.Type type = item.GetType();
-                if (type == typeof(Rectangle)) 
-                {
-                    Rectangle temp = (Rectangle)item;
-                    double area = temp.Area();
-                    WriteLine($"This is Rectangle. Type {type}. Area {area} "); 
-                }
-
-                if (type == typeof(Triangle))
-                {
-                    Triangle temp = (Triangle)item;
-                    double area = temp.Area();
-                    WriteLine($"This is Triangle. Type {type}. Area {area} ");
-                }
-
-                if (type == typeof(Circle))
-                {
-                    Circle temp = (Circle)item;
-                    double area = temp.Area();
-                    WriteLine($"This is Circle. Type {type}. Area {area} ");
-                }
-            }
+            foreach (var item in figures.figures) 
+            { WriteLine($"This is {item.GetType()}. Area {item.Area()}"); }
+                        
         }
     }
 }
