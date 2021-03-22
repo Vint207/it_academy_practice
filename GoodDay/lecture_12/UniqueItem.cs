742c6434d74e8a7283d86813790d3f445d28f505
Admin <Admin@BEST-KOMP>
2021-03-22 19:36:16 +0300

    1

---
+++ GoodDay/lecture_12/UniqueItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_12
+{
+    class UniqueItem
+    {
+        static int staticId;
+        public int id;
+
+        static UniqueItem() { staticId = 0; }
+        public UniqueItem() { staticId++; id = staticId; }
+        public UniqueItem(int id) { staticId = id;  this.id = staticId; }
+    }
+}
