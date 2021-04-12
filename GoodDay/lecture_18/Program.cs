aab0b1db2d38b5f20236f743ecb9cee457e754f1
Admin <Admin@BEST-KOMP>
2021-04-12 19:59:20 +0300

    2

--- GoodDay/lecture_18/Program.cs
+++ GoodDay/lecture_18/Program.cs
@@ -8,20 +8,27 @@
         static void Main(string[] args)
         {
             Motorcycle motorcycle = new();
+            MotorcyclesArray motorcyclesArray = new();
             MotorcycleDelegat1 del = new MotorcycleDelegat1(MethodToDelegat);
 
-            Motorcycle FirstOrDefault(MotorcyclesArray list, MotorcycleDelegat1 arg)
-            {
-                foreach (var item in list.arr)
-                {
-                    if (arg.Invoke(item, val: 50000, state: 1))
-                    {
-                        return item;
-                    }
-                }
-                return null;
-            }
+            FirstOrDefault(motorcyclesArray, del, val: 50000, state: 1);
+            FirstOrDefault(motorcyclesArray, del, country: "China", state: 2);
+            FirstOrDefault(motorcyclesArray, del, val: 50000, country: "China", state: 3);
+
         }
+
+        static Motorcycle FirstOrDefault(MotorcyclesArray list, MotorcycleDelegat1 arg, int val = 0, string country = null, int state = 0)
+        {
+            foreach (var item in list.arr)
+            {
+                if (arg.Invoke(item, val, country, state))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
         static bool MethodToDelegat(Motorcycle obj, int val = 0, string country = null, int state = 0)
         {
             switch (state)
@@ -40,7 +47,7 @@
                     }
                     return false;
 
-                case 3  :
+                case 3:
                     if (obj.MadeIn == country && obj.MadeIn == country)
                     {
                         return true;
